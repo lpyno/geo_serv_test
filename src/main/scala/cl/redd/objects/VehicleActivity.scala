3b2e4c333b93512edd90324d15bb5e6270f29d3d
lpino <lpino@redd.cl>
2017-11-22 17:45:33 -0300

    getFleetsByUser - fleet activity Ok ( old format )
    Signed-off-by: lpino <lpino@redd.cl>

--- src/main/scala/cl/redd/objects/VehicleActivity.scala
+++ src/main/scala/cl/redd/objects/VehicleActivity.scala
@@ -1,7 +1,10 @@
 package cl.redd.objects
 
-case class VehicleActivity ( 		actives			 : Option[Int] = None,
+case class VehicleActivity (
+                            id     : Option[Int] = None,
+                            status : Option[Map[String,Int]] = None
+                             /* actives			 : Option[Int] = None,
                                 inactives		 : Option[Int] = None,
                                 inactives24h : Option[Int] = None,
-                                moving			 : Option[Int] = None
+                                moving			 : Option[Int] = None*/
                            )
